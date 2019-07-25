74ad63becec90a7bcb6da6dc1ae4971453e72b51
OmarM <oemiii16@gmail.com>
2019-07-24 19:09:24 -0500

    pushing to repo

--- GammaRayInformation/src/app/components/flashcards/flashcards.component.ts
+++ GammaRayInformation/src/app/components/flashcards/flashcards.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-flashcards',
   templateUrl: './flashcards.component.html',
-  styleUrls: ['./flashcards.component.css']
+  styleUrls: ['./flashcards.component.css'],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class FlashcardsComponent implements OnInit {
 
@@ -11,5 +24,12 @@ export class FlashcardsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  flip: string = 'inactive';
+  toggleFlip() {
+    this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
+  }
+
+  
 
 }
