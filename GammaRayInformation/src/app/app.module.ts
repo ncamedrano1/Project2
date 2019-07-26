9f806d416c69e5db899e4225874420389ab0583e
Tom Luna <tomas.e.luna@gmail.com>
2019-07-25 22:40:33 -0500

    updated home page

--- GammaRayInformation/src/app/app.module.ts
+++ GammaRayInformation/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +30,7 @@ import { ProfileComponent } from './components/profile/profile.component';
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
