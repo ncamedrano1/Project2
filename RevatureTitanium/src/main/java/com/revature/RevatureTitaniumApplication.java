75272c971ae435b2715a52892bced1e576802d3f
joseph highe <jshighe@alumni.fullsail.edu>
2019-07-27 22:19:27 -0500

    updating RevatureTitanium on master branch

--- RevatureTitanium/src/main/java/com/revature/RevatureTitaniumApplication.java
+++ RevatureTitanium/src/main/java/com/revature/RevatureTitaniumApplication.java
@@ -2,10 +2,11 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class RevatureTitaniumApplication {
 
 	public static void main(String[] args) {
--- RevatureTitanium/src/main/java/com/revature/repository/SubjectTypesRepository.java
+++ RevatureTitanium/src/main/java/com/revature/repository/SubjectTypesRepository.java
@@ -1,15 +1,11 @@
 package com.revature.repository;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.revature.models.SubjectTypes;
 
 @Repository
-public interface SubjectTypesRepository extends JpaRepository<SubjectTypes, Integer>  {
+public interface SubjectTypesRepository extends JpaRepository<SubjectTypes, Integer> {
 
 }
