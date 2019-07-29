8fcd54b5ecdb32bf8fa133dc1005dfae9b7d6443
joseph highe <jshighe@alumni.fullsail.edu>
2019-07-29 14:28:38 -0500

    Changing DatasoureDriverClassName to oracle.jdbc.driver.OracleDriver, from oracle.jdbc.OracleDriver

--- RevatureTitanium/src/main/java/com/titanium/configuration/JdbcConfig.java
+++ RevatureTitanium/src/main/java/com/titanium/configuration/JdbcConfig.java
@@ -83,7 +83,7 @@ public class JdbcConfig implements DataSource {
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
 		dataSource.setUrl("jdbc:oracle:thin:@jhmydb.cguopvp0dama.us-east-2.rds.amazonaws.com:1521:orcl");
 		dataSource.setUsername("project2Manager");
 		dataSource.setPassword("p2Password");
