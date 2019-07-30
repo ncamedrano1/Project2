4b2468f0fdb36d84194ba4d215732e2b69cdc4d2
OmarM <oemiii16@gmail.com>
2019-07-30 18:02:50 -0500

    resolving merge issues with Maven branch

---
+++ RevatureTitanium/src/test/java/com/revature/test/frontend/TestRunner.java
@@ -0,0 +1,9 @@
+package com.revature.test.frontend;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+public class TestRunner {
+	
+}
