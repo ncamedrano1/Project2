4f88ef8769dce21e9e054b07f145a9320d8c37cc
joseph highe <jshighe@alumni.fullsail.edu>
2019-08-01 21:12:41 -0500

    fixing bug to fix Jenkins Build

--- RevatureTitanium/src/main/java/com/titanium/controllers/SubjectQuestionsControllerImpl.java
+++ RevatureTitanium/src/main/java/com/titanium/controllers/SubjectQuestionsControllerImpl.java
@@ -2,13 +2,11 @@ package com.titanium.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.testng.log4testng.Logger;
 
 import com.titanium.aspect.LoggingAspect;
 import com.titanium.models.QuizQuestions;
--- RevatureTitanium/src/main/java/com/titanium/controllers/SubjectTypesControllerImpl.java
+++ RevatureTitanium/src/main/java/com/titanium/controllers/SubjectTypesControllerImpl.java
@@ -2,7 +2,6 @@ package com.titanium.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
